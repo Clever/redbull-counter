01c2e85730bc77010008545b83a792f803c69ca8
Alex Zylman <azylman@Alexs-MacBook-Pro-2.local>
2015-04-15 01:32:25 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# redbull-counter
-web server that you notify when you have a redbull
+# redbullbot
+
+Slack bot that can be informed + track redbull consumption, for an individual or a team.
+
+It requires three environment variables:
+
+* `SLACK_TOKEN`: Your Slack token, found by making a Hubot integration in Slack
+* `SLACK_DOMAIN`: Your team's Slack domain, e.g. `https://<team>.slack.com`
+* `SLACK_USERNAME`: The username you configured the bot for in Slack, e.g. `redbull`
+
+Then you can message it with or without your team:
+
+```
+@redbull rack me up
+@redbull rack one up
+@redbull rack me up for the api team
+@redbull rack me up for the API team
+@redbull rack one up for the API team
+```
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"gopkg.in/Clever/kayvee.v2"
+)
+
+func redbull(user, team string) {
+	log.Printf(kayvee.FormatLog("redbullbot", kayvee.Info, "drink", map[string]interface{}{
+		"user": user,
+		"team": strings.ToLower(team),
+	}))
+}
+
+func main() {
+	token := os.Getenv("SLACK_TOKEN")
+	domain := os.Getenv("SLACK_DOMAIN")
+	username := os.Getenv("SLACK_USERNAME")
+
+	client, err := NewClient(token, domain, username)
+	if err != nil {
+		panic(err)
+	}
+	client.Respond("rack (me|one) up$", func(msg *Message, params [][]string) {
+		author, err := msg.Author()
+		if err != nil {
+			panic(err)
+		}
+		redbull(author, "")
+		msg.Respond("Thanks for telling me! Hope it gave you wings. :)")
+	})
+	client.Respond("rack (me|one) up for the (.*) team", func(msg *Message, params [][]string) {
+		author, err := msg.Author()
+		if err != nil {
+			panic(err)
+		}
+		team := params[0][2]
+		redbull(author, team)
+		msg.Respond("Thanks for telling me! Hope it gave you wings. :)")
+	})
+	panic(client.Run())
+}
